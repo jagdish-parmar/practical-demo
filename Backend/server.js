4fa9694393ec77facf0ef4c99af3b80b3bb54183
Jagdish <39767913+jagdish-parmar@users.noreply.github.com>
2020-02-22 16:33:10 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,33 @@
-sds
+let express = require("express");
+let bodyParser = require("body-parser");
+let compression = require("compression");
+let helmet = require("helmet");
+let cors = require("cors")
+let logger = require("morgan");
+let admin = require("firebase-admin");
+let settings = require("./utils/settings")
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+let middleware = require("./middleware/index");
+let router = require("./router/index");
+app.use(logger("dev"));
+app.use(compression());
+app.use(helmet());
+
+app.use(cors({ origin: '*' }));
+let PORT = process.env.PORT || 4000;
+
+admin.initializeApp({
+    credential: admin.credential.cert(settings),
+    databaseURL: "https://myapp-a5ac5.firebaseio.com",
+    storageBucket: "gs://myapp-a5ac5.appspot.com"
+});
+middleware(app);
+router(app);
+
+
+app.listen(PORT, () => {
+    console.log(`Server Listinig On ${PORT}`)
+});
