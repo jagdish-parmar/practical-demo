1a9f90599e9f087d6183f8b8d5b4c88d38cf1c34
Jagdish <39767913+jagdish-parmar@users.noreply.github.com>
2020-02-22 17:24:03 +0530

    Update home.component.ts

--- Fronted/src/app/page/home/home.component.ts
+++ Fronted/src/app/page/home/home.component.ts
@@ -6,8 +6,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   public homes: any = [];
